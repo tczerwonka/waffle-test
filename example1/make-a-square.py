b7cfb1ab613552b3169bb70e458641dec426a693
Waffle Turtle Test <tczerwonka@gmail.com>
2023-12-27 18:53:09 -0600

    added a stop with distance sensor
    Added code to stop using the distance sensor.

--- example1/make-a-square.py
+++ example1/make-a-square.py
@@ -40,20 +40,54 @@ async def stopped_turn(angle):
     motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
     await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, angle, 150, -150, stop=motor.SMART_BRAKE)
 #########################################################################
+#distance detect
+#drives forward until a certain distance from an object, with sensor in front of robot
+#argument is in centimeters
+#########################################################################
+async def distance_detect_forward(stop_distance):
+    distance_sensor_illuminate()
+    stop_distance = (stop_distance * 10)
+    motor_pair.unpair(motor_pair.PAIR_1)
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+    while True:
+        if distance_sensor.distance(port.B)>stop_distance or distance_sensor.distance(port.B) == -1: motor_pair.move(motor_pair.PAIR_1,0)
+        else:
+            motor_pair.stop(motor_pair.PAIR_1)
+            break
+    distance_sensor.clear(port.B)
+    motor_pair.stop(motor_pair.PAIR_1)
+
+######################################################################
+#Turn on the distance sensor lights
+#takes no arguments
+######################################################################
+def distance_sensor_illuminate():
+    distance_sensor.set_pixel(port.B, 0, 0, 100)
+    distance_sensor.set_pixel(port.B, 0, 1, 100)
+    distance_sensor.set_pixel(port.B, 1, 0, 100)
+    distance_sensor.set_pixel(port.B, 1, 1, 100)
+
+
+#########################################################################
 #this is the 'main' program -- this is where everything starts
 # for i in range(times of loops): is a loop function. Make sure code under the loop is tabbed out
 #########################################################################
 async def main():
-    for i in range(4): #this is a loop. The orange number is how many times you want the loop to happen. The code you want repeted has to be indented
+    for i in range(2):#this is a loop. The orange number is how many times you want the loop to happen. The code you want repeted has to be indented
         await stopped_turn(90) #This makes the robot turn 90 degrees, or a right angle, to the right
         await run_motors(25) #this makes the robot move forward 25cm
         #Now lets reverse
-    for i in range(4):#another loop
-            await stopped_turn(-90) #adding a negitive infront of a stopped_turn command will make it turn left
-            await run_motors(-25) #a negitive parameter for run_motors command will make it go backward
+    for i in range(2):#another loop
+            await stopped_turn(-90) # a negitive value for a stopped_turn command will make it turn left
+            await run_motors(-25) # a negitive argument for run_motors command will make it run backward
+
+    #let's drive forward now until we are within 5 cm of a wall, then stop and light up the sensor.
+    await distance_detect_forward(5)
+    await distance_sensor_illuminate
 
 
 
     sys.exit(0)
 
 runloop.run(main())
+
